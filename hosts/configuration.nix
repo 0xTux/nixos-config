50200cfa63466ec187bbfae22f70521e1a94b0dc
0xTux <0xtux@pm.me>
2023-09-06 19:00:24 +0530

    updated keys

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -19,7 +19,7 @@
       isNormalUser = true;
       extraGroups = [ "networkmanager" "wheel" "storage" "audio" ];
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+OzPUe2ECPC929DqpkM39tl/vdNAXfsRnmrGfR+X3D 0xtux@pm.me"
+        ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL+OzPUe2ECPC929DqpkM39tl/vdNAXfsRnmrGfR+X3D 0xtux@pm.me''
       ];
     };
   };
