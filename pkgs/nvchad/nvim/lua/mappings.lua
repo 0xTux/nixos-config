be44073ccb48b2b7a9d46cdb2949655112957265
0xTux <0xtux@pm.me>
2024-03-21 04:59:58 +0530

    add binding for buffers

--- pkgs/nvchad/nvim/lua/mappings.lua
+++ pkgs/nvchad/nvim/lua/mappings.lua
@@ -24,6 +24,10 @@ local toggleTreesj = function()
   require("treesj").toggle()
 end
 
+local closeAllBuffer = function()
+  require("nvchad.tabufline").closeAllBufs()
+end
+
 map({ "n", "t" }, "<F7>", toggleTerm, { desc = "Toggle Floating Terminal" })
 map({ "n", "t" }, "<F8>", toggleLazygit, { desc = "Toggle Lazygit" })
 map("n", "<leader>Sl", "<cmd>SessionManager! load_last_session<cr>", { desc = "Load last session" })
@@ -37,12 +41,20 @@ map("n", "<leader>m", toggleTreesj, { desc = "Toggle Treesitter Join" })
 map("n", "<leader>o", "<cmd>Outline<cr>", { desc = "Toggle Outline" })
 map("n", "<leader>tn", "<cmd>Boole increment<cr>", { desc = "Increase value" })
 map("n", "<leader>tp", "<cmd>Boole decrement<cr>", { desc = "Decrease value" })
+map("n", "<leader>X", closeAllBuffer, { desc = "Close all buffers" })
 
 -- DAP
 map("n", "<C-b>", "<cmd>DapToggleBreakpoint<cr>", { desc = "Toggle break point" })
 map("n", "<C-c>", "<cmd>DapContinue<cr>", { desc = "Continue" })
 map("n", "<C-x>", "<cmd>DapTerminate<cr>", { desc = "Terminate" })
 map("n", "<C-o>", "<cmd>DapStepOver<cr>", { desc = "Step over" })
+
+-- Go To Tab
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
 
 if g.neovide then
   local zoomInNeovide = function()
