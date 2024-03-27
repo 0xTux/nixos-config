007c82cdb4985be6c3ab6bf5b5751b6b4c135e36
0xTux <0xtux@pm.me>
2024-03-27 19:58:23 +0530

    add qemu and virt-viewer

--- modules/nixos/virtualisation/qemu.nix
+++ modules/nixos/virtualisation/qemu.nix
@@ -4,12 +4,20 @@
   ...
 }: {
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
   };
 
   users.users.${username}.extraGroups = ["libvirtd"];
 
   environment.systemPackages = with pkgs; [
     virt-manager
+    virt-viewer
   ];
 }
