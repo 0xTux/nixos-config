89d7b51a3ce6f4af501f9cba2c8bcb4ae81d8f51
0xTux <0xtux@pm.me>
2024-05-20 00:33:24 +0530

    remove comments and awesome overrides

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,16 +2,7 @@
   additions = final: _prev: import ../pkgs {pkgs = final;};
 
   modifications = final: prev: {
-    #vscode = prev.vscode.overrideAttrs (oldAttrs: rec {
-    #  src = (builtins.fetchTarball {
-    #    url = "https://code.visualstudio.com/sha/download?build=stable&os=linux-x64";
-    #    sha256 = "09hy3nw1bblrl0blzpwv492gy0hwwbaq4csx00mcymskginz1wyx";
-    #  });
-    #  version = "latest";
-    #  buildInputs = oldAttrs.buildInputs;
-    #});
-
-    awesome = inputs.nixpkgs-f2k.packages.${prev.system}.awesome-git.override {lua = prev.lua53Packages.lua;};
+    awesome = inputs.nixpkgs-f2k.packages.${prev.system}.awesome-git;
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
