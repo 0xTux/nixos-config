1085957a68406aae913893c4229977868cfb2af7
0xTux <0xtux@pm.me>
2024-08-05 02:55:52 +0530

    update starship config

--- modules/home-manager/starship/default.nix
+++ modules/home-manager/starship/default.nix
@@ -2,20 +2,40 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = ''[󰊠  ](bold green)$directory(bold blue)$git_branch$git_status[❯](bold yellow)[❯](bold purple)[❯](bold blue) '';
+      format = ''$os$hostname$directory$rust$golang$solidity$nodejs(bold blue)$git_branch$git_status[❯](bold yellow)[❯](bold purple)[❯](bold blue) '';
       scan_timeout = 60;
       add_newline = false;
-
       line_break.disabled = true;
-      directory.style = "bold blue";
 
-      cmd_duration.format = "[$duration]($style) ";
+      os = {
+        format = "[$symbol  ]($style)";
+        style = "bold green";
+        disabled = false;
+        symbols.NixOS = "󰊠";
+      };
+      directory.style = "bold blue";
       git_branch.format = "[$symbol$branch]($style) ";
-      nodejs.format = "[$symbol($version )]($style)";
-      rust.format = "[$symbol($version )]($style)";
+      hostname = {
+        ssh_only = false;
+        format = "[$ssh_symbol$hostname]($style) ";
+        style = "bold green";
+        disabled = false;
+      };
+      rust = {
+        format = "[$symbol]($style)";
+        symbol = " ";
+      };
       golang = {
-        format = "[$symbol($version )]($style)";
-        symbol = " ";
+        format = "[$symbol]($style)";
+        symbol = "� ";
+      };
+      solidity = {
+        format = "[$symbol]($style)";
+        symbol = "� ";
+      };
+      nodejs = {
+        format = "[$symbol]($style)";
+        symbol = "� ";
       };
     };
   };
