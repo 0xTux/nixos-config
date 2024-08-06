a216302e730657d5a6d826f57c96f3f9b015d245
0xTux <0xtux@pm.me>
2024-08-06 09:48:00 +0530

    update start up apps

--- pkgs/awesome/awesome/startup.lua
+++ pkgs/awesome/awesome/startup.lua
@@ -5,8 +5,16 @@ awful.spawn.with_shell([[
     barrierc -f --debug INFO --display :0 --name esoteric --disable-crypto 192.168.1.2:24800 &
   fi
 ]])
--- awful.spawn.single_instance("firefox")
--- awful.spawn.single_instance("discord")
--- awful.spawn.single_instance("spotify")
--- awful.spawn.single_instance("copyq")
--- awful.spawn.single_instance("blueman-applet")
+
+awful.spawn.single_instance("firefox", {}, function(c)
+  return c.class == "firefox"
+end)
+awful.spawn.single_instance("copyq", {}, function(c)
+  return c.class == "copyq"
+end)
+awful.spawn.single_instance("blueman-applet", {}, function(c)
+  return c.class == ".blueman-manager-wrapped"
+end)
+awful.spawn.single_instance("discord", {}, function(c)
+  return c.class == "discord"
+end)
