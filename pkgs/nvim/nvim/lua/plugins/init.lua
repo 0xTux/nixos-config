b1a38177bfc37126b6bf239a09064fa8f5d94d4b
0xTux <0xtux@pm.me>
2024-08-30 14:18:12 +0530

    change event to very lazy

--- pkgs/nvim/nvim/lua/plugins/init.lua
+++ pkgs/nvim/nvim/lua/plugins/init.lua
@@ -189,7 +189,7 @@ local plugins = {
   },
   {
     "OXY2DEV/markview.nvim",
-    lazy = false,
+    event = "VeryLazy",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
