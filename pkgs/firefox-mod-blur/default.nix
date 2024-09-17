739d526df29a12af3ac3cf5df9a4c368083a0e01
0xTux <0xtux@pm.me>
2024-09-18 02:32:32 +0530

    update nix flake

--- pkgs/firefox-mod-blur/default.nix
+++ pkgs/firefox-mod-blur/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
     owner = "datguypiko";
     repo = "Firefox-Mod-Blur";
     rev = "refs/heads/master";
-    sha256 = "sha256-7FsYPsWjzH6CVp1Yq5zx5RIAbLTg5u98RSmhycj1hY0=";
+    sha256 = "sha256-3d+dJVvOqMO+egFrEY1fdCeFpkVvsRvXoOs3rPnI/sc=";
   };
 
   installPhase = ''
