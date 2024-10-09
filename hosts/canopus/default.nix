65d482072d327fe2edd7ef28bcf4d39219c120f0
0xTux <0xtux@pm.me>
2024-10-09 11:48:54 +0530

    add kernel params for quite boot

--- hosts/canopus/default.nix
+++ hosts/canopus/default.nix
@@ -59,6 +59,17 @@
       ];
     };
 
+    kernelParams = [
+      "quiet"
+      "loglevel=3"
+      "systemd.show_status=auto"
+      "udev.log_level=3"
+      "rd.udev.log_level=3"
+      "vt.global_cursor_default=0"
+    ];
+    consoleLogLevel = 0;
+    initrd.verbose = false;
+
     kernelPackages = pkgs.linuxPackages_zen;
     supportedFilesystems = ["ntfs"];
 
