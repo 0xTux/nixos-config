c2f1ca9d23aaf3b9947b5dcb967247b302b073e7
0xTux <0xtux@pm.me>
2024-10-18 13:54:14 +0530

    update tailscale host

--- pkgs/awesome/awesome/ui/wibar/module/tailscale.lua
+++ pkgs/awesome/awesome/ui/wibar/module/tailscale.lua
@@ -3,7 +3,7 @@ local watch = require("awful.widget.watch")
 local wibox = require("wibox")
 
 local tailscale = wibox.widget.textbox("Home: NA")
-watch("bash -c \"tailscale ping pi | awk '{print $NF}'\"", 10, function(_, stdout)
+watch("bash -c \"tailscale ping vega | awk '{print $NF}'\"", 10, function(_, stdout)
   tailscale.text = "Home:" .. " " .. stdout
 end)
 
